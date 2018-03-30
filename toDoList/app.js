7fbbd2231201a2a617f49b58b7b129c739346c0b
jealvarez123 <jealvarez123@gmail.com>
2018-03-30 14:54:53 -0500

    Added functionality to the text field

--- toDoList/app.js
+++ toDoList/app.js
@@ -1,2 +1,64 @@
 console.log("Can I kick it");
- console.log(" Can I kick it ");
+
+//This allows the  close button to hide the item selected to remove from the List
+let close =
+
+document.getElementsByClassName('close');
+let j;
+
+for (j=0; j < close.length; j++) {
+  close[j].onclick = () => {
+    let section = this.parentElement;
+    section.style.display = "none";
+  }
+}
+
+// This adds a 'Checked' symbol when clicking on the selected item
+
+let check = document.querySelector('ul');
+check.addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('checked');
+  }
+}, false);
+
+// This adds a new item to the list when the 'Add It!' button is clicked
+
+function newElement() {
+  let li = document.createElement('li');
+  let inputValue = document.getElementById('myInput').value;
+  let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert('Be productive! Add something!');
+    } else {
+      document.getElementById('myUL').appendChild(li);
+    }
+    document.getElementById('myInput').value = '';
+
+    let span = document.createElement('SPAN');
+    var txt = document.createTextNode('\u00D7');
+
+    span.className = 'close';
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (let i = 0; i < close.length; i++) {
+      close[i].onclick = () => {
+        let section = this.parentElement;
+        section.style.display = "none";
+      }
+      }
+}
+
+// This creates a close button and adds it to each item added
+
+let theList = document.getElementsByTagName('LI');
+let i;
+for (i=0; i <theList.length; i++) {
+
+  let span = document.createElement('SPAN');
+  span.className = 'close';
+  span.appendChild(txt);
+  theList[i].appendChild(span);
+}
